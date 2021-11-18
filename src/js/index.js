9f3c226a4f75b0fbc015f7a583a69211ed423460
Dmitry Zatonsky <83658538+DmitryZatonsky@users.noreply.github.com>
2021-11-18 19:56:43 +0200

    символ рейтинга после запятой

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@ const api = new Api();
 
 //Разметка карточек по запросу на бэк
 api.fetchMovie().then(data => {
-  onRatingFixedNumber();// нужно както залить в шаблон на место vote_average
+  onRatingFixedNumber(data);
   refs.cardList.insertAdjacentHTML('beforeend', createCardMovies(data));
 });
 
@@ -20,31 +20,40 @@ api.fetchGenres().then(data => {
   return data;
 });
 
-  //добавляет в рейтинг знак после запятой
-function onRatingFixedNumber() {
-  api.fetchMovie()
-    .then(data => {
-      data.forEach(el => el.vote_average.toFixed([1]))
-    })
+// перезаписывает значение рейтинга
+function onRatingFixedNumber(data) {
+  data.forEach(el => {
+    return (el.vote_average = number(el.vote_average));
+  });
+  // console.log(data);
+}
+
+//добавляет к одинарному символу знак после запятой
+function number(qval) {
+  if (qval.toString().length === 1) {
+    return '.0'.padStart(3, qval);
+  }
+  return qval;
 }
 
 //отрезает от даты релиза символы после 4-го знака
-api.fetchMovie().then(data => {
-  const year = data.map(el => {
-    const dataStr = el.release_date
-    // .slice(0, 4)
-    // console.log(dataStr);
-    return dataStr;
-  })
-  // console.log(year)
-})
+// api.fetchMovie().then(data => {
+//   const year = data.map(el => {
+//     const dataStr = el.release_date;
+//     // .slice(0, 4)
+//     // console.log(dataStr);
+//     return dataStr;
+//   });
+//   // console.log(year)
+// });
 
-//Заменяет значение жанра на строку с именем жанра  
-  // const genreIds = data.map(el => el.genre_ids);
-  // console.log(genreIds)
-  // const comparison = genreIds.forEach(element => {
-    // console.log(api.fetchGenres(element).then(console.log))
-    // api.fetchGenres(element).then();
-  // });
+//Заменяет значение жанра на строку с именем жанра
+// const genreIds = data.map(el => el.genre_ids);
+// console.log(genreIds)
+// const comparison = genreIds.forEach(element => {
+// console.log(api.fetchGenres(element).then(console.log))
+// api.fetchGenres(element).then();
+// });
 // })
+
 
