1de4b3a912cfab88f0864d015c0272c2c5d8c00f
IhorStorozhok <storozhokigor@gmail.com>
2021-11-18 14:38:15 +0200

    coomit 1

--- src/js/index.js
+++ src/js/index.js
@@ -5,36 +5,29 @@ import Api from './apiFetch';
 
 const refs = {
   cardList: document.querySelector('.cards-movie-list'),
-}
-
-
-
-
-
+};
 
 const api = new Api();
 
 //Разметка карточек по запросу на бэк
-api.fetchMovie()
-  .then((data) => {
-    const genreIds = data.map(el => el.genre_ids)
-    // console.log(genreIds)
+api.fetchMovie().then(data => {
+  const genreIds = data.map(el => el.genre_ids);
+  // console.log(genreIds)
 
-    const comparison = genreIds.forEach(element => {
-      // console.log(api.fetchGenres(element).then(console.log))
-      api.fetchGenres(element).then()
-    });
+  const comparison = genreIds.forEach(element => {
+    // console.log(api.fetchGenres(element).then(console.log))
+    api.fetchGenres(element).then();
+  });
 
-    // console.log(comparison)
-    return refs.cardList.insertAdjacentHTML('beforeend', createCardMovies(data))
-  })
-    // .catch(console.log(error))
+  // console.log(comparison)
+  return refs.cardList.insertAdjacentHTML('beforeend', createCardMovies(data));
+});
+// .catch(console.log(error))
 
-
-api.fetchGenres().then((data) => {
-    // console.log(data)
+api.fetchGenres().then(data => {
+  // console.log(data)
   // const genreName = data.map(el => el);
-  const genreName = data
+  const genreName = data;
   // console.log(genreName)
-  return genreName
-}) 
+  return genreName;
+});
