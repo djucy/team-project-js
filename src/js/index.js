72b643c81847e51fc0e85a5307f39b83eebc2388
Yuliia Danylenko <yvdanilenko@gmail.com>
2021-11-18 23:29:56 +0200

    стартовая разметка modal-team

--- src/js/index.js
+++ src/js/index.js
@@ -2,39 +2,32 @@ import '../sass/main.scss';
 //Сюда импортируем все свои джеес модули
 import createCardMovies from '../templates/cardMovie.hbs';
 import Api from './apiFetch';
-
+import modalTeam from '../js/modal-team';
 const refs = {
   cardList: document.querySelector('.cards-movie-list'),
-}
-
-
-
-
-
+};
 
 const api = new Api();
 
 //Разметка карточек по запросу на бэк
-api.fetchMovie()
-  .then((data) => {
-    const genreIds = data.map(el => el.genre_ids)
-    // console.log(genreIds)
+api.fetchMovie().then(data => {
+  const genreIds = data.map(el => el.genre_ids);
+  // console.log(genreIds)
 
-    const comparison = genreIds.forEach(element => {
-      // console.log(api.fetchGenres(element).then(console.log))
-      api.fetchGenres(element).then()
-    });
+  const comparison = genreIds.forEach(element => {
+    // console.log(api.fetchGenres(element).then(console.log))
+    api.fetchGenres(element).then();
+  });
 
-    // console.log(comparison)
-    return refs.cardList.insertAdjacentHTML('beforeend', createCardMovies(data))
-  })
-    // .catch(console.log(error))
+  // console.log(comparison)
+  return refs.cardList.insertAdjacentHTML('beforeend', createCardMovies(data));
+});
+// .catch(console.log(error))
 
-
-api.fetchGenres().then((data) => {
-    // console.log(data)
+api.fetchGenres().then(data => {
+  // console.log(data)
   // const genreName = data.map(el => el);
-  const genreName = data
+  const genreName = data;
   // console.log(genreName)
-  return genreName
-}) 
+  return genreName;
+});
