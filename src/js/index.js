58cca49fd575d765d30812cdc2a030d1bf5eff30
Yuliia Danylenko <yvdanilenko@gmail.com>
2021-11-22 01:22:10 +0200

    разметка modalTeam

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,4 @@ import '../sass/main.scss';
 import markupCardMovie from './markupCardMovie';
 import inputMoviesSerch from './inputMoviesSerch';
 import pageSwitch from './pageSwitch';
+import modalTeam from './modal-team';
