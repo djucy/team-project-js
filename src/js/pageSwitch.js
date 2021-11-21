450a5df9d7b463c64eb41d385650156b3c821592
IhorStorozhok <storozhokigor@gmail.com>
2021-11-21 22:32:20 +0200

    my commit

--- src/js/pageSwitch.js
+++ src/js/pageSwitch.js
@@ -1,10 +1,10 @@
 const refs = {
-    homeLink: document.querySelectorAll('.home-link'),
-    libraryLink: document.querySelector('.library-link'),
-    libraryHeader: document.querySelector('.js-library__header'),
-    homeHeader: document.querySelector('.js-home__header'),
-    libraryContainer: document.querySelector('.js-cards-movie-list'),
-    libraryHomeLink:document.querySelector('.library-home__link')
+  homeLink: document.querySelectorAll('.home-link'),
+  libraryLink: document.querySelector('.library-link'),
+  libraryHeader: document.querySelector('.js-library__header'),
+  homeHeader: document.querySelector('.js-home__header'),
+  libraryContainer: document.querySelector('.js-cards-movie-list'),
+  libraryHomeLink: document.querySelector('.library-home__link'),
 };
 
 refs.libraryLink.addEventListener('click', onLibraryDraw);
@@ -12,15 +12,13 @@ refs.libraryHomeLink.addEventListener('click', onHomeDraw);
 
 // Отрисовка библиотеки, пока она пустая
 function onLibraryDraw() {
-    refs.libraryHeader.classList.remove('header-hidden');
-    refs.homeHeader.classList.add('header-hidden');
-    refs.libraryContainer.innerHTML = '';
+  refs.libraryHeader.classList.remove('header-hidden');
+  refs.homeHeader.classList.add('header-hidden');
+  refs.libraryContainer.innerHTML = '';
 }
 
 // Возврат на домашнюю страницу
 function onHomeDraw() {
-     refs.homeHeader.classList.remove('header-hidden');
-    refs.libraryHeader.classList.add('header-hidden');
-
-} 
-
+  refs.homeHeader.classList.remove('header-hidden');
+  refs.libraryHeader.classList.add('header-hidden');
+}
