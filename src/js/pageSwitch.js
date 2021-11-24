e114cce2411d3833f81edb31ca9e401ed7c70219
kim <82406962+KristinaKim6686@users.noreply.github.com>
2021-11-24 12:50:35 +0300

    home-building-fail

--- src/js/pageSwitch.js
+++ src/js/pageSwitch.js
@@ -1,11 +1,15 @@
-const refs = {
-  homeLink: document.querySelectorAll('.home-link'),
-  libraryLink: document.querySelector('.library-link'),
-  libraryHeader: document.querySelector('.js-library__header'),
-  homeHeader: document.querySelector('.js-home__header'),
-  libraryContainer: document.querySelector('.js-cards-movie-list'),
-  libraryHomeLink: document.querySelector('.library-home__link'),
-};
+import Api from './apiFetch';
+import onError from './markupCardMovie';
+// import createCardMovies from '../templates/cardMovie.hbs';
+import markupCardMovie from './markupCardMovie';
+import onCreateMarkup from './markupCardMovie';
+import fetchMovie from './apiFetch';
+import MY_KEY from './apiFetch';
+import BASE_URL from './apiFetch';
+import refs from './refs';
+
+
+const api = new Api();
 
 refs.libraryLink.addEventListener('click', onLibraryDraw);
 refs.libraryHomeLink.addEventListener('click', onHomeDraw);
@@ -14,11 +18,50 @@ refs.libraryHomeLink.addEventListener('click', onHomeDraw);
 function onLibraryDraw() {
   refs.libraryHeader.classList.remove('header-hidden');
   refs.homeHeader.classList.add('header-hidden');
-  refs.libraryContainer.innerHTML = '';
+  refs.cardsMovieList.innerHTML = '';
 }
 
 // Возврат на домашнюю страницу
-function onHomeDraw() {
+
+
+  function  onHomeDraw() {
   refs.homeHeader.classList.remove('header-hidden');
-  refs.libraryHeader.classList.add('header-hidden');
+    refs.libraryHeader.classList.add('header-hidden');
+    fetchHomePage();
 }
+
+function fetchHomePage() {
+  api.fetchMovie()
+  .then(data => {
+    onCreateMarkup(data);
+    console.log(data);
+  })
+    .catch(onError);
+  
+  api
+  .fetchGenres()
+  .then(genres => {
+    genres.forEach(el => {
+      genresArrayStr.push(el);
+    });
+  })
+  .catch(onError);
+
+  const genresArrayStr = [];
+  
+}
+
+// то что работает у Димы, просто поставить как есть//
+
+// refs.libraryHomeLink.addEventListener('click', onHomeDraw);
+//   function  onHomeDraw() {
+//   refs.homeHeader.classList.remove('header-hidden');
+//     refs.libraryHeader.classList.add('header-hidden');
+//   api.fetchMovie()
+//   .then(data => {
+//     // onCreateMarkup(data);
+//     console.log(data);
+//   })
+//   .catch(onError);
+
+// }
--- src/js/refs.js
+++ src/js/refs.js
@@ -5,4 +5,11 @@ export default {
   searchForm: document.querySelector('.js-movies-search'),
   loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 // =============================================================
+
+  
+  homeLink: document.querySelectorAll('.home-link'),
+  libraryLink: document.querySelector('.library-link'),
+  libraryHeader: document.querySelector('.js-library__header'),
+  homeHeader: document.querySelector('.js-home__header'),
+  libraryHomeLink: document.querySelector('.library-home__link'),
 };
