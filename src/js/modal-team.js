b00c38c034b9189a5b25ed58715ff1685a6ca5c0
Yuliia Danylenko <yvdanilenko@gmail.com>
2021-11-26 19:15:02 +0200

    modal-team: адаптив

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,15 +1,34 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+import refs from './refs';
+const refsTeam = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+  backdropModalTeam: document.querySelector('.backdrop-team'),
+};
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+function onModalTeamBtn() {
+  refsTeam.openModalBtn.addEventListener('click', toggleModal);
+  refsTeam.closeModalBtn.addEventListener('click', toggleModal);
+  refsTeam.backdropModalTeam.addEventListener('click', onBackdropClick);
+}
 
-  function toggleModal() {
-    document.body.classList.toggle('body-hidden');
-    refs.modal.classList.toggle('visually-hidden');
+function toggleModal() {
+  refsTeam.modal.classList.toggle('visually-hidden');
+}
+
+//закрытие при нажатии на Esc
+document.addEventListener('keydown', e => {
+  if (e.code === 'Escape') {
+    toggleModal();
   }
-})();
+});
+
+//закрытие при нажатии на backdrop
+
+function onBackdropClick(evt) {
+  if (refsTeam.backdropModalTeam === evt.target) {
+    toggleModal();
+  }
+}
+
+onModalTeamBtn();
