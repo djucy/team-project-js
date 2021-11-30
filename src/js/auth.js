40193497f9b43132549cddf5abf79bc528ef7f39
IhorStorozhok <storozhokigor@gmail.com>
2021-11-30 13:34:10 +0200

    submit

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,7 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,signOut } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { getDatabase, ref, set, onValue, child, get } from 'firebase/database';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+import { onCloseModalClick } from './signup';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,12 +28,23 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-const db = getDatabase();
+const db = getFirestore();
+
+//get data
+
+const querySnapshot = getDocs(collection(db, 'Films')).then(snapShot => console.log(snapShot.docs));
+console.log(querySnapshot);
+
+//lisend auth changed
+onAuthStateChanged(auth, user => {
+  console.log(user);
+});
 
 //sign up
 
 const signForm = document.querySelector('#sign-form');
 const submitButton = signForm.querySelector('.pseudo-button');
+
 submitButton.addEventListener('click', () => {
   signForm.requestSubmit();
 });
@@ -44,8 +65,28 @@ signForm.addEventListener('submit', e => {
 
 //logout
 const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-    e.preventDefault();
-    signOut()
+logout.addEventListener('click', e => {
+  e.preventDefault();
+  signOut(auth);
+  onCloseModalClick();
+});
 
-})
+const loginForm = document.querySelector('#login-form');
+
+const loginButton = document.querySelector('.login-pseudo-button');
+console.log(loginButton);
+loginButton.addEventListener('click', () => {
+  loginForm.requestSubmit();
+});
+loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+  //get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-pass'].value;
+
+  signInWithEmailAndPassword(auth, email, password).then(cred => {
+    console.log(cred.user);
+  });
+  loginForm.reset();
+  onCloseModalClick();
+});
